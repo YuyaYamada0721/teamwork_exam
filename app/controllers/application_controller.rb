51de15ed75631ad7bea107008c9c011015c61a87
yuya yamada <yuya19920721@gmail.com>
2021-06-29 07:12:10 +0900

    6/28のワーク(アソシエーションについて)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  include SessionsHelper
+  before_action :login_required
+
+  private
+
+  def login_required
+    redirect_to new_session_path unless current_user
+  end
+
+  def ensure_current_user
+    if logged_in?
+      unless current_user.id == params[:id].to_i
+        flash[:notice] = '権限がありません'
+        redirect_to blogs_path
+      end
+    else
+      redirect_to new_user_path, notice: 'ログインが必要です'
+    end
+  end
 end
--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,13 +1,12 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: %i[ show edit update destroy ]
 
-  # GET /blogs or /blogs.json
   def index
     @blogs = Blog.all
   end
 
-  # GET /blogs/1 or /blogs/1.json
   def show
+    @favorite = current_user.favorites.find_by(blog_id: @blog.id)
   end
 
   # GET /blogs/new
@@ -21,17 +20,12 @@ class BlogsController < ApplicationController
 
   # POST /blogs or /blogs.json
   def create
-    @blog = Blog.new(blog_params)
-
-    respond_to do |format|
+    @blog = current_user.blogs.build(blog_params)
       if @blog.save
-        format.html { redirect_to @blog, notice: "Blog was successfully created." }
-        format.json { render :show, status: :created, location: @blog }
+        redirect_to @blog, notice: "Blog was successfully created."
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
+        render :new, status: :unprocessable_entity
       end
-    end
   end
 
   # PATCH/PUT /blogs/1 or /blogs/1.json
